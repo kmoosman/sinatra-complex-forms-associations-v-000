2f7987539c6ccaa08b4f305948a6ef2e3873255a
Katie Moosman <moosman.katie@gmail.com>
2019-03-13 19:53:57 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,12 +12,9 @@ class PetsController < ApplicationController
 
   post '/pets' do
     @pet = Pet.create(name: params[:pet][:name])
-    # binding.pry
     if !!params[:pet][:owner_id]
       @pet.owner_id = params[:pet][:owner_id].first
-      # binding.pry
     elsif !params["owner"]["name"].empty?
-      # binding.pry
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
     @pet.save
